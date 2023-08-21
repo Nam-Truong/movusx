7b0e79a550e301c272e6b06082692d106d6c8498
Nam Truong <nam.truong@macbook2019.pro>
2023-08-21 18:06:44 +0930

    change the JPG extension to jpg

---
+++ docs/public/_nuxt/ProseCodeInline.9a634295.js
@@ -0,0 +1,1 @@
+import{_ as o,o as n,c as r,N as c}from"./entry.d1f9c29d.js";const s={};function t(e,a){return n(),r("code",null,[c(e.$slots,"default")])}const _=o(s,[["render",t]]);export{_ as default};
---
+++ docs/public/_nuxt/ProseOl.3297c755.js
@@ -0,0 +1,1 @@
+import{_ as o,o as r,c as s,N as t}from"./entry.d1f9c29d.js";const c={};function n(e,l){return r(),s("ol",null,[t(e.$slots,"default")])}const _=o(c,[["render",n]]);export{_ as default};
